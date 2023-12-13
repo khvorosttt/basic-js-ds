4f948503e5ee4254813a25a936f33c31e01f91ee
khvorosttt <khvorosttt@yandex.by>
2023-12-13 21:46:33 +0300

    feat: add method implementation root

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -12,8 +12,7 @@ class BinarySearchTree {
     this._root = null;
   }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root;
   }
 
   add(/* data */) {
