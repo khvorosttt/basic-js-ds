316ea796033d5b9c23b95f4b5c5627f42e408270
khvorosttt <khvorosttt@yandex.by>
2023-12-14 02:24:48 +0300

    feat: add constructor Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,6 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+
+  constructor() {
+    this.top = null;
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
