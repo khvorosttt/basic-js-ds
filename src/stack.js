1be6ec30684a8aa29b7849c72f73b49e10fb8039
khvorosttt <khvorosttt@yandex.by>
2023-12-14 03:01:05 +0300

    feat: add constructor Stack

--- src/stack.js
+++ src/stack.js
@@ -13,6 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+
+  constructor() {
+    this._stack = [];
+  }
   push(/* element */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
