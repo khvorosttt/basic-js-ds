e25a5a5c29b0898c203552ad14c405836f6cc68d
khvorosttt <khvorosttt@yandex.by>
2023-12-14 20:13:10 +0300

    feat: add method implementation removeKFromList

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if(!l){
+    return null;
+  }
+  if(l.value === k) {
+      l = l.next;
+  }
+  let currentElem = l;
+  while(currentElem && currentElem.next) {
+    if(currentElem.next.value === k) {
+      currentElem.next = currentElem.next.next;
+    } else {
+        currentElem = currentElem.next;
+    }
+  } 
+  return l;
 }
 
 module.exports = {
